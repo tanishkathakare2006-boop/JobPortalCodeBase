609e90bfc2978c39e1b45321d8046b26549656c7
tanishkathakare2006 <tanishkathakare2006@gmail.com>
2026-07-07 18:38:46 +0530

    Getting Started with Projects

---
+++ section1 (1)/section1/src/test/java/com/s2p/section1/Section1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s2p.section1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
